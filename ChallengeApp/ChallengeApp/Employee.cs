993ad1142dcd31d56f9c05485c6ac30b33a52bbc
dawid.jurczyk97@interia.pl <dawid.jurczyk97@interia.pl>
2023-05-04 20:39:53 +0200

    Homework day 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace ChallengeApp
+{
+    class Employee
+    {
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public  string FirstName { get; private set; }
+        public  string LastName { get; private set; }
+        public  int Age { get; private set; }
+
+        private List<int> grade = new List<int>();
+
+
+        public void AddGrade(int grade)
+        {
+            this.grade.Add(grade);
+        }
+
+        public int Result
+        {
+            get
+            {
+                return this.grade.Sum();
+            }
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ChallengeApp
+﻿
+
+namespace ChallengeApp
 {
     internal class Program
     {
@@ -6,27 +8,72 @@
 
         static void Main(string[] args)
         {
+            Employee denis = new Employee("Denis", "Smith", 32);
+            Employee john = new Employee("John", "West", 44);
+            Employee rick = new Employee("Rick", "Willman", 56);
 
-            User user1 = new User("Adam", "12312fsdfsd");
-            User user2 = new User("Dawid", "12312fsdfsd");
-            User user3 = new User("Pati", "12312fsdfsd");
-            User user4 = new User("Natala", "12312fsdfsd");
+            denis.AddGrade(3);
+            denis.AddGrade(7);
+            denis.AddGrade(8);
+            denis.AddGrade(10);
+            denis.AddGrade(4);
+
+            john.AddGrade(5);
+            john.AddGrade(7);
+            john.AddGrade(9);
+            john.AddGrade(8);
+            john.AddGrade(9);
+
+            rick.AddGrade(10);
+            rick.AddGrade(1);
+            rick.AddGrade(8);
+            rick.AddGrade(7);
+            rick.AddGrade(9);
+
+            List<Employee> employees = new List<Employee>()
+            {  denis, john, rick, };
+
+            int highestGrade = -1;
+
+            Employee employeeWithHighestGrade = null;
+
+            foreach (var i in employees)
+            {
+                if (i.Result > highestGrade)
+                {
+                    highestGrade = i.Result;
+                    employeeWithHighestGrade = i;
+                }
+            }
+
+            Console.WriteLine("Employee grades are:");
+            Console.WriteLine($"Denis - {denis.Result}");
+            Console.WriteLine($"John - {john.Result}");
+            Console.WriteLine($"Rick - {rick.Result}");
+            Console.WriteLine("___________________________________________");
+            Console.WriteLine("Employee with the highest grade is:");
+            Console.WriteLine($"{employeeWithHighestGrade.FirstName} {employeeWithHighestGrade.LastName}");
+            Console.WriteLine($"{employeeWithHighestGrade.Age} years old");
+            Console.WriteLine($"grade: {employeeWithHighestGrade.Result}");
 
 
 
-            user1.AddScore(5);
-            user1.AddScore(2);
-            var result = user1.Result;
-            Console.WriteLine(result);
-            var name = User.GameName;
-            
+
+
+
+
 
 
 
         }
 
 
-      
+
+
+
+
+
+
 
     }
 
